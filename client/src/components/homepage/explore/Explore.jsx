291b6ecee57f1ea65bb17897683607752ba471b2
mozarcxiaye-dev <mozarcxiaye.dev@gmail.com>
2025-01-29 17:30:14 +0545

    added shadcn to the project and also added carousel

--- client/src/components/homepage/explore/Explore.jsx
+++ client/src/components/homepage/explore/Explore.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Text from '../../../units/Text';
 import ButtonswithArrow from '../../ReusableComponent/buttons/ButtonswithArrow';
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 const Explore = () => {
   return (
-    <div className='flex h-[50vh] justify-center items-center bg-[#fff]'>
+    <div className='flex flex-col md:flex-row h-[50vh] justify-center items-center bg-[#fff]'>
       {/* Explore description */}
-      <div>
+      <div className="text-center md:text-left md:mr-10">
         <Text className='text-[#F82BA9]'>Premium Gifts</Text>
         <Text>
           Experience <span className='text-[#F82BA9]'>Premium Gifts</span> and Products
@@ -15,7 +23,32 @@ const Explore = () => {
           <ButtonswithArrow text='Explore Now' />
         </div>
       </div>
-      {/* Carousel card component */}
+      
+      {/* Carousel component */}
+      <div className="mt-6 md:mt-0">
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          className="w-full max-w-sm"
+        >
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <span className="text-3xl font-semibold">{index + 1}</span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
     </div>
   );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        dancingScript: ['Dancing Script', 'cursive'],
-      },
-    },
+  	extend: {
+  		fontFamily: {
+  			dancingScript: [
+  				'Dancing Script',
+  				'cursive'
+  			]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
   plugins: [
     require('daisyui'),
-  ],
+      require("tailwindcss-animate")
+],
 }
