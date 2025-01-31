6b43600235b56c0625dd7c49b8d71d114443d896
mozarcxiaye-dev <mozarcxiaye.dev@gmail.com>
2025-01-31 15:44:36 +0545

    added loader to the Giffz

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,34 @@
-import { StrictMode, useState } from 'react'
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import './App.css'
-import HomeLayout from './pages/home/HomeLayout'
-import HomePage from './pages/home/HomePage'
-// import ErrorPage from './components/common/Error/ErrorPage'
+import React, { StrictMode, useState, useEffect } from 'react'; // Import StrictMode
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css';
+import HomeLayout from './pages/home/HomeLayout';
+import HomePage from './pages/home/HomePage';
+import Loader from './components/common/loader/Loader';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
 
   return (
-    <>
-      <StrictMode>
+    <StrictMode> {/* Now properly imported */}
+      {loading ? (
+        <Loader />
+      ) : (
         <BrowserRouter>
           <Routes>
-            {/* Homepage */}
-            <Route path='/' element={<HomeLayout />}>
-              <Route path='/' element={<HomePage />}></Route>
+            <Route path="/" element={<HomeLayout />}>
+              <Route index element={<HomePage />} />
             </Route>
-
-            {/* Error Page */}
-            {/* <Route path="*" element={<ErrorPage />} /> */}
-
           </Routes>
-
-
         </BrowserRouter>
-      </StrictMode>
-    </>
-  )
+      )}
+    </StrictMode>
+  );
 }
 
-export default App
+export default App;
